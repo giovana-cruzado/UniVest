581f908e069e52160f2ca232776a0df7b652602a
giovana-cruzado <giovanarusbe@gmail.com>
2025-08-13 12:21:55 -0300

    Parte da filtragem finalizada.

--- UniVest/Data/AppDbSeed.cs
+++ UniVest/Data/AppDbSeed.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using UniVest.Models;
 using UniVest.Data;
+using Microsoft.AspNetCore.Identity;
 
 namespace UniVest.Data
 {
@@ -131,6 +132,71 @@ namespace UniVest.Data
                     ModalidadeId = 1
                 }
             };
+
+            #region Populate Roles - Perfis de Usuário
+            List<IdentityRole> roles = new()
+        {
+            new IdentityRole() {
+               Id = "0b44ca04-f6b0-4a8f-a953-1f2330d30894",
+               Name = "Administrador",
+               NormalizedName = "ADMINISTRADOR"
+            },
+            new IdentityRole() {
+               Id = "bec71b05-8f3d-4849-88bb-0e8d518d2de8",
+               Name = "Funcionário",
+               NormalizedName = "FUNCIONÁRIO"
+            },
+            new IdentityRole() {
+               Id = "ddf093a6-6cb5-4ff7-9a64-83da34aee005",
+               Name = "Cliente",
+               NormalizedName = "CLIENTE"
+            },
+        };
+            builder.Entity<IdentityRole>().HasData(roles);
+            #endregion
+
+            #region Populate Usuário
+            List<Usuario> usuarios = new() {
+            new Usuario(){
+                Id = "ddf093a6-6cb5-4ff7-9a64-83da34aee005",
+                Email = "giovanascruzado@gmail.com",
+                NormalizedEmail = "GIOVANASCRUZADO@GMAIL.COM",
+                UserName = "Giovana",
+                NormalizedUserName = "GIOVANA",
+                LockoutEnabled = true,
+                EmailConfirmed = true,
+                Nome = "Giovana Souza Cruzado",
+                Foto = "/img/usuarios/ddf093a6-6cb5-4ff7-9a64-83da34aee005.png"
+            }
+        };
+            foreach (var user in usuarios)
+            {
+                PasswordHasher<IdentityUser> pass = new();
+                user.PasswordHash = pass.HashPassword(user, "123456");
+            }
+            builder.Entity<Usuario>().HasData(usuarios);
+            #endregion
+
+            #region Populate UserRole - Usuário com Perfil
+            List<IdentityUserRole<string>> userRoles = new()
+        {
+            new IdentityUserRole<string>() {
+                UserId = usuarios[0].Id,
+                RoleId = roles[0].Id
+            },
+            new IdentityUserRole<string>() {
+                UserId = usuarios[0].Id,
+                RoleId = roles[1].Id
+            },
+            new IdentityUserRole<string>() {
+                UserId = usuarios[0].Id,
+                RoleId = roles[2].Id
+            }
+        };
+            builder.Entity<IdentityUserRole<string>>().HasData(userRoles);
+            #endregion
         }
     }
 }
+    
+
